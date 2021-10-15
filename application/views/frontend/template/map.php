9b10ef16783ecb91f500be626b7f8bf51d5ae08d
misbahulms <misbahul.munir@student.untan.ac.id>
2021-10-15 19:08:21 +0700

    update lagi
    map belum fix

--- application/views/frontend/template/map.php
+++ application/views/frontend/template/map.php
@@ -1,7 +1,6 @@
 <script>
 
 	var mymap = L.map('mapid').setView([-0.043402, 109.241779], 15);
-
 	var Layer=L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
 		maxZoom: 18,
 		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
@@ -12,74 +11,94 @@
 	});
 	mymap.addLayer(Layer);
 
-	function iconByName(name) {
-		return '<i class="icon" style="background-color:'+name+';border-radius:50%"></i>';
-	}
-	
-	function featureToMarker(feature, latlng) {
-	return L.marker(latlng, {
-		icon: L.divIcon({
-			className: 'marker-'+feature.properties.amenity,
-			html: iconByName(feature.properties.amenity),
-			iconUrl: '../images/markers/'+feature.properties.amenity+'.png',
-			iconSize: [25, 41],
-			iconAnchor: [12, 41],
-			popupAnchor: [1, -34],
-			shadowSize: [41, 41]
-		})
-	});
+	var info = L.control();
+
+info.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'info');
+    this.update();
+    return this._div;
+};
+
+info.update = function (props) {
+    this._div.innerHTML = '<h4>Informasi Pemilik Lahan</h4>' +  (props ?
+        '<b> Nama Pemilik : </b>' + props.Nama + '<br/><b> Luas Lahan : </b>' + props.Luas + ' Ha</br> <b> Nama Poktan : </b>' + props.Poktan + '</br> <b> Nama Desa : </b>' + props.Desa
+        : 'Informasi Poktan');
+};
+
+info.addTo(mymap);
+
+<?php foreach ($poktan as $pkt) : ?>
+		var myStyle<?= $pkt['id']?> = {
+            "color": "<?= $pkt['warna']?>",
+            "weight": 1,
+            "opacity": 1
+        };
+<?php endforeach;?>
+
+function style(feature) {
+    return {
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7,
+        fillColor: getColor(feature.properties.KECAMATAN)
+    };
 }
 
-	<?php foreach ($poktan as $pkt) {
-		$id = $pkt['idPoktan'];
-		$site = site_url();
-		$url = "<a href='".$site."FrontPoktan/detail/".$id."'><small>Lihat Detail</small></a>";
+function highlightFeature(e) {
+    var layer = e.target;
 
-		$arrayPok[]='{
-			name: "'. $pkt['namaPoktan'] .'",
-			icon: iconByName("'.$pkt['warna'].'"),
-			layer: new L.GeoJSON.AJAX(["'. base_url() . 'assets/frontend/geojson/' . $pkt['geojson'] . '"], 
-			{style: myStyle' . $pkt['id'] . ' = {
-				"color": "' . $pkt['warna'] . '",
+    layer.setStyle({
+        weight: 3,
+        color: '#666',
+        dashArray: '',
+        fillOpacity: 0.7
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
+    }
+
+    info.update(layer.feature.properties);
+}
+
+function resetHighlight(e) {
+    var layer = e.target;
+
+    layer.setStyle({
+        weight: 2,
+        opacity: 1,
+        color: 'red',
+        dashArray: '3'
+    });
+	info.update();
+}
+
+function zoomToFeature(e) {
+    mymap.fitBounds(e.target.getBounds());
+}
+
+function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: zoomToFeature
+    });
+}
+
+<?php foreach ($poktan as $pkt) : ?>
+    
+        var jsonTest = new L.GeoJSON.AJAX(["<?= base_url() ?>assets/geojson/<?= $pkt['geojson'] ?>"],
+            {
+                style: myStyle<?= $pkt['id']?> = {
+				"color": "<?= $pkt['warna']?>",
 				"weight": 1,
 				"opcaity": 1
 			},
-			pointToLayer: featureToMarker }).addTo(mymap).bindPopup("<b>Nama Gapoktan :</b> '. $pkt['namaGapoktan'] .'</br><b>Nama Poktan :</b> '. $pkt['namaPoktan'] .' </br> '.$url.'")
-			}';
-		 } ?>
-
-	var overLayers = [{
-		group: "Layer Poktan",
-		layers: [
-		<?= implode(',', $arrayPok);?>
-		]
-	}];
-
-var baseLayers = [
-	{
-		group: "Map Layers",
-		icon: iconByName('parking'),
-		collapsed: true,
-		layers: [
-			{
-				name: "Google Satelite",
-				layer: L.tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}',{
-    					maxZoom: 20,
-    					subdomains:['mt0','mt1','mt2','mt3']
-				})
-			},
-			{
-				name: "Open Cycle Map",
-				layer: L.tileLayer('https://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png')
-			}			
-		]
-	}
-];
-
-var panelLayers = new L.Control.PanelLayers(baseLayers, overLayers, {
-	collapsibleGroups: true
-});
-
-mymap.addControl(panelLayers);
+                onEachFeature: onEachFeature
+                // onEachFeature: popUp,
+            }).addTo(mymap).bindPopup('<a href="<?= site_url('petani/tambah_petani')?>">Lihat Data</a>');
+<?php endforeach; ?>
 
 </script>
